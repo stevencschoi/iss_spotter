926048cfc3dab7b0132d8729afba954b301df8a6
Steven Choi <stevencschoi87@gmail.com>
2020-02-13 23:53:40 +0000

    refactored to include error handling

--- iss.js
+++ iss.js
@@ -12,7 +12,17 @@ const url = 'https://api.ipify.org?format=json';
 const fetchMyIP = function(callback) { 
   // use request to fetch IP address from JSON API
   request(url, (error, response, body) => {
-    if (error) callback(error);
+     // error can be set if invalid domain, user is offline, etc.
+  if (error) {
+    callback(error, null);
+    return;
+  }
+  // if non-200 status, assume server error
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
     if (body) {
       const data = JSON.parse(body);
       callback(error, data.ip);
