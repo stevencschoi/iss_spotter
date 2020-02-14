bc2c94c01e73d99d2f6692b000fc3fdb37d76e8f
Steven Choi <stevencschoi87@gmail.com>
2020-02-14 00:02:29 +0000

    ran eslint

--- iss.js
+++ iss.js
@@ -1,25 +1,32 @@
 const request = require('request');
 const url = 'https://api.ipify.org?format=json';
 
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request(url, (error, response, body) => {
-     // error can be set if invalid domain, user is offline, etc.
-  if (error) {
-    callback(error, null);
-    return;
-  }
-  // if non-200 status, assume server error
-  if (response.statusCode !== 200) {
-    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    callback(Error(msg), null);
-    return;
-  }
+  // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
     if (body) {
       const data = JSON.parse(body);
       callback(error, data.ip);
     }
   });
-}
+};
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
